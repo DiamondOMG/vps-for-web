7abec2cc062a81c626a4402f8efd432f04360e82
Mond Naja <omgdigital.developer.02@gmail.com>
2026-02-24 16:49:27 +0700

    v

---
+++ js/imConfig.js
@@ -0,0 +1,19 @@
+export const immersalParams = {
+    developerToken: "21aac2dfb57011cfea947357710d22e33ae389fc819adf96f43e9b4abd5a4377",
+    mapIds: [142000],
+    continuousLocalization: true,
+    continuousInterval: 16, // ms
+    imageDownScale: 0.25,
+    solverType: 1,
+    /* DEBUG with fixed focal length
+    deviceData: {
+      model: "Apple MacBook Pro",
+      width: 960,
+      height: 720,
+      fx: 980,
+      fy: 980,
+      ox: 480,
+      oy: 360
+    }
+    */
+  };
